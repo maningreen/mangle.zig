7394dab8949eb127727ffdc7ac27a66de94fde42
maningreen <flibbers@protonmail.com>
2026-07-07 14:22:38 -0600

    simple registry complete

--- src/engine.zig
+++ src/engine.zig
@@ -7,66 +7,23 @@ const util = @import("util.zig");
 const StructField = std.builtin.Type.StructField;
 const StructAttrs = std.builtin.Type.StructField.Attributes;
 
+const ArrayType = std.ArrayList;
+
 pub const System = struct {
-    Types: Signature,
-    /// it's ensured `anytype` will be a tuple of slices ordered by `Types`
+    requirements: Signature,
+    /// it's ensured `anytype` will be of self.Type()
     process: fn (anytype, RegistryInformation) ProcessError!void,
+    pub const ArrayType = @This().ArrayType;
+
+    pub inline fn Type(comptime self: System) type {
+        return @This().ArrayType(self.requirements.GetType());
+    }
 };
 
 pub const RegistryInformation = struct {
     gpa: std.mem.Allocator,
     io: std.Io,
 };
-
-/// Returns a struct, with an array of the necessary data for the type
-pub fn Archetype(sig: Signature) type {
-    return struct {
-        const Self = @This();
-
-        pub const Signature = sig;
-        pub const DataType = sig.GetType();
-
-        /// an array list of all the data,
-        /// - data is optional, if it's missing at the top level
-        ///   then it's assumed it's uninitialized.
-        /// - data is differentiated by the type, and nothing else
-        ///   if the data has multiple items of the same type,
-        ///   it's recommended making type aliases instead
-        /// example of recommendation
-        /// ```
-        /// DataType = .{ i32, i32 }
-        /// ```
-        /// Do
-        /// ```
-        /// DataType = .{ Health, Damage }
-        /// ```
-        data: std.ArrayList(?DataType),
-
-        pub inline fn qualifies(_: Self, item: anytype) bool {
-            return comptime sig.qualifies(item);
-        }
-
-        /// returns a pointer to the newly allocated *unset* data
-        /// Tuple form.
-        pub fn allocItem(self: *Self, gpa: std.mem.Allocator) !*DataType {
-            return self.data.addOne(gpa);
-        }
-
-        /// appends an item to the list
-        pub fn appendItem(self: *Self, gpa: std.mem.Allocator, item: DataType) !void {
-            try self.data.append(gpa, item);
-        }
-
-        /// no deallocation.
-        pub fn swapPop(self: *Self, index: usize) void {
-            self.data.swapRemove(index);
-        }
-
-        pub fn deinit(self: *Self, gpa: std.mem.Allocator) void {
-            self.data.deinit(gpa);
-        }
-    };
-}
 
 pub const Signature = struct {
     items: []const type,
@@ -79,9 +36,8 @@ pub const Signature = struct {
 
     /// returns whether or not a structure (if not structure returns whether or not is contained)
     /// qualifies for the signature
-    pub fn qualifies(comptime self: Signature, item: anytype) bool {
+    pub inline fn qualifies(comptime self: Signature, comptime T: type) bool {
         comptime {
-            const T = @TypeOf(item);
             if (@typeInfo(T) == .@"struct")
                 for (@typeInfo(T).@"struct".fields) |j| {
                     if (!std.mem.containsAtLeastScalar2(type, self.items, j.type, 1))
@@ -94,62 +50,56 @@ pub const Signature = struct {
     }
 };
 
-/// `types` should be all the types the engine will attach,
-/// `types` are also infered from the `systems`
-pub fn Registry(comptime types: []const type, comptime systems: []const System) type {
+/// `types` should be all the types the engine will utilize,
+/// `types` *will not* be infered by systems.
+pub fn Registry(comptime types: []const type, comptime requestedSystems: []const System) type {
     comptime {
-        // step one: infer types from systems
-        const allSystemTypes = getSystemTypes(systems);
+        // create structure of arrays
+        var valueTypes: [types.len]type = undefined;
+        for (types, 0..) |T, i|
+            valueTypes[i] = ArrayType(T);
 
-        // step two: concat all of them into one list
-        const allTypes = comptimeConcatNoRepeats(type, types, allSystemTypes);
-        _ = allTypes;
+        const DataType = @Tuple(&valueTypes);
 
-        return struct {};
-    }
-}
+        return struct {
+            /// the raw data of all the types, a tuple of @This().arrayTypes
+            /// recommended to not access manually
+            data: DataType,
+            info: RegistryInformation,
 
-inline fn getSystemTypes(comptime systems: []const System) []const type {
-    comptime {
-        if (systems.len == 0) {
-            return &.{};
-        } else {
-            return systems[0].Types.items ++ getSystemTypes(systems[1..]);
-        }
-    }
-}
+            pub fn init(io: std.Io, gpa: std.mem.Allocator) @This() {
+                var data: DataType = undefined;
+                inline for (arrayTypes, 0..) |T, i|
+                    @field(data, std.fmt.comptimePrint("{d}", .{i})) = T.empty;
 
-/// Super innefficient.
-/// TODO: fix that
-fn comptimeConcatNoRepeats(comptime T: type, comptime a: []const T, comptime b: []const T) []T {
-    comptime {
-        var array: []T = &.{};
-        outer: for (a ++ b) |item| {
-            for (array) |i| {
-                if (item == i)
-                    // item already appears
-                    continue :outer
-                else {
-                    array = array ++ &.{item};
-                }
+                return .{
+                    .data = data,
+                    .info = .{
+                        .gpa = gpa,
+                        .io = io,
+                    },
+                };
             }
-        }
-        return array;
+
+            pub fn deinit(self: *@This()) void {
+                inline for (0..types.len) |i|
+                    self.data[i].deinit(self.info.gpa);
+            }
+
+            pub fn addValue(self: *@This(), value: anytype) std.mem.Allocator.Error!void {
+                const T = @TypeOf(value);
+                const i = comptime for (allTypes, 0..) |J, i| {
+                    if (T == J) break i;
+                } else @compileError("Error, type \"" ++ @typeName(T) ++ "\" is not in the Registry!");
+
+                try self.data[i].append(self.info.gpa, value);
+            }
+
+            pub const systems: [requestedSystems.len]System = &requestedSystems;
+            pub const arrayTypes = valueTypes;
+            pub const allTypes = types;
+        };
     }
 }
-
-pub const Entity = struct {
-    /// the current archetype for which this entity qualifies
-    record: *anyopaque,
-
-    /// the signature of the archetype
-    sig: Signature,
-
-    /// the name of the entity
-    name: []const u8,
-
-    /// the index in the archetype which the data lies
-    index: usize,
-};
 
 pub const ProcessError = error{ProcessError} || std.mem.Allocator.Error;
