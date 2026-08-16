5722d97b2ebeb1bbee0e4fe07f0409eebaf0b553
maningreen <flibbers@protonmail.com>
2026-08-15 18:32:47 -0600

    removed runtime, more systems testing

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,5 @@
 const std = @import("std");
 const engine = @import("engine.zig");
-const Runtime = @import("runtime.zig");
 const rl = @import("raylib");
 
 comptime {
@@ -9,15 +8,28 @@ comptime {
 
 const Radius = f32;
 const Pos = rl.Vector2;
+const Gravity = void;
 const Colour = rl.Color;
 
 const testSystem: engine.System = .{
     .requirements = .{ .items = &.{ Radius, Colour, Pos } },
     .process = (struct {
-        fn process(val: anytype, _: engine.RegistryInformation) engine.ProcessError!void {
+        fn process(val: *anyopaque, _: engine.RegistryInformation) engine.ProcessError!void {
             const T = testSystem.requirements.GetType();
-            const circle: T = @as(T, val);
-            rl.drawCircleV(circle[2], circle[0], circle[1]);
+            const circlePtr: *T = @alignCast(@ptrCast(val));
+            const circle = circlePtr.*;
+            rl.drawCircleV(circle.@"2", circle.@"0", circle.@"1");
+        }
+    }).process,
+};
+
+const gravSystem: engine.System = .{
+    .requirements = .{ .items = &.{ Pos, Gravity } },
+    .process = (struct {
+        fn process(val: *anyopaque, _: engine.RegistryInformation) engine.ProcessError!void {
+            const T = testSystem.requirements.GetType();
+            const objPtr: *T = @alignCast(@ptrCast(val));
+            objPtr[0] -= 10;
         }
     }).process,
 };
@@ -32,19 +44,22 @@ pub fn main(init: std.process.Init) !void {
         r: Radius,
         c: Colour,
         p: Pos,
+        g: Gravity,
     };
-    const Reg = engine.Registry(&.{T}, &.{testSystem});
+    const Reg = engine.Registry(&.{T}, &.{testSystem, gravSystem});
 
     var rt: Reg = .init(io, init.gpa);
     defer rt.deinit();
-    try rt.addValue(T{.c = .white, .p = .one(), .r = 30});
 
     rl.initWindow(1920, 1080, "test");
     defer rl.closeWindow();
 
+    rl.beginDrawing();
+    try rt.addValue(T{.c = .blue, .p = rl.Vector2.one().scale(30), .r = 30, .g = void{}});
+    try rt.process(0);
+    rl.endDrawing();
+
+
     while (!rl.windowShouldClose()) {
-        rl.beginDrawing();
-        rl.drawCircle(0, 0, 30, .ray_white);
-        rl.endDrawing();
     }
 }
