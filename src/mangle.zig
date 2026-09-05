28b956aed27246b230c251a6994de4b25bd95072
maningreen <flibbers@protonmail.com>
2026-09-05 11:53:15 -0600

    events are now implimented

--- src/mangle.zig
+++ src/mangle.zig
@@ -15,12 +15,7 @@ test {
     std.testing.refAllDecls(@This());
 }
 
-inline fn processWrapper(
-    comptime Sys: type,
-    comptime T: type,
-    arg: *T,
-    regInfo: anytype,
-) !void {
+inline fn applySystem(comptime Sys: type, comptime T: type, comptime function: @EnumLiteral(), comptime inlined: bool, value: *T, extraArgs: anytype) !void {
     const info = switch (@typeInfo(T)) {
         .@"struct" => |i| i,
         else => @compileError("Error: Type '" ++ @typeName(T) ++ "' is not a struct!"),
@@ -33,11 +28,13 @@ inline fn processWrapper(
                     .owned => {
                         comptime if (@typeInfo(field.type) != .@"struct") continue;
 
-                        try processWrapper(
+                        try applySystem(
                             Sys,
                             field.type,
-                            &@field(arg, field.name),
-                            regInfo,
+                            function,
+                            inlined,
+                            &@field(value, field.name),
+                            extraArgs,
                         );
                     },
                     .leaf, .dissolve => continue,
@@ -56,7 +53,11 @@ inline fn processWrapper(
     }
 
     const Named = @field(Sys, system.fields.signature.name).NamedType(Eroded);
-    try @field(Sys, system.fields.function.name)(Named, @as(*Named, @ptrCast(@alignCast(arg))), regInfo);
+    return @call(
+        if (inlined) .always_inline else .auto,
+        @field(Sys, @tagName(function)),
+        .{ Named, @as(*Named, @ptrCast(value)) } ++ extraArgs,
+    );
 }
 
 /// `types` should be all the types the registry will utilize,
@@ -151,11 +152,13 @@ pub fn Registry(comptime types: []const type, comptime requestedSystems: []const
                     const arr = self.getArrayFromType(T);
                     inline for (systems) |Sys|
                         for (arr.items) |*value|
-                            try processWrapper(
+                            try applySystem(
                                 Sys,
                                 T,
+                                .process,
+                                false,
                                 value,
-                                &self.info,
+                                .{&self.info},
                             );
                 }
                 self.drop();
@@ -250,7 +253,7 @@ pub fn Registry(comptime types: []const type, comptime requestedSystems: []const
                 ///
                 /// **NOTE**:
                 ///     - Is an interrupt, other events are processed on call
-                pub inline fn emit(self: *RegistryT, eventData: anytype) !void {
+                pub inline fn emit(self: *RegistryInformation, eventData: anytype) !void {
                     return @as(*RegistryT, @fieldParentPtr("info", self)).emit(eventData);
                 }
             };
@@ -289,11 +292,27 @@ pub fn Registry(comptime types: []const type, comptime requestedSystems: []const
                     list.clearRetainingCapacity();
                 }
             }
-            
-            /// Internal function. Loops through all systems and calls `recieve` if available
+
+            // / Internal function. Loops through all systems and calls `recieve` if available
             fn emit(self: *RegistryT, event: anytype) !void {
                 inline for (systems) |Sys| {
-                    if (@hasDecl(Sys, system.fields.recieve.name)) {
+                    if (!@hasDecl(Sys, system.fields.recieve.name)) continue;
+                    inline for (allTypes, 0..) |T, i| {
+                        if (!@field(Sys, system.fields.signature.name).qualifies(T))
+                            continue;
+                        for (self.data[i].items) |*value| {
+                            try applySystem(
+                                Sys,
+                                T,
+                                .recieve,
+                                true,
+                                value,
+                                .{
+                                    event,
+                                    self.info,
+                                },
+                            );
+                        }
                     }
                 }
             }
