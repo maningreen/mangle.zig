5ff8221c1bd37553735cb34e06815abb8a616166
maningreen <flibbers@protonmail.com>
2026-09-09 22:52:26 -0600

    flake edit

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "A simple development environent for the Fisht Fighting game";
+  description = "A simple development environent for the mangle engine";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
